62387314a9e2d7d0815d149c6c81506888005bf8
rahullahoria <rahul_lahoria@yahoo.com>
2017-05-20 17:42:55 +0530

    first

--- api/app.php
+++ api/app.php
@@ -23,60 +23,12 @@ $app->response->headers->set('Content-Type', 'application/json');
 
 /* Starting routes */
 
-$app->get('/service_provider','getServiceProviderByType');
-$app->get('/service_provider/:id','getServiceProvider');
+//auth
+$app->post('/auth', 'userAuth');//user,org,temp_user{mobile,mac}
+$app->post('/users', 'regUser');
+$app->post('/ads', 'createAds');
 
-// services resource
-$app->get('/services','getAllServices');
-$app->get('/services/:id','getServicesById');
-$app->get('/service/:id','getAllServiceProviders');
-/*$app->get('/service_provider/:id','getServiceProviderById');*/
-
-
-// service provider resouce
-$app->post('/service_provider', 'insertServiceProvider');
-$app->put('/service_provider/:id','updateServiceProvider');
-$app->post('/service_provider/:id','updateServiceProvider');
-
-$app->post('/auth', 'userAuth');
-
-// search api
-$app->get('/search/:keywords','search');
-
-// invoice api
-$app->post('/service_provider/:id/invoice', 'insertServiceProviderInvoice');
-$app->get('/service_provider/:id/invoice', 'getServiceProviderInvoice');
-
-// expanses api
-$app->post('/service_provider/:id/expanse', 'insertServiceProviderExpanse');
-$app->get('/service_provider/:id/expanse', 'getServiceProviderExpanses');
-$app->get('/service_provider/:id/expanse_types', 'getExpansesTypes');
-
-// take feedback request api
-$app->post('/service_provider/:id/feedback_request', 'insertServiceProviderFeedbackRequest');
-
-// create campaigning request
-$app->post('/service_provider/:id/campaigning_request', 'createCampaignRequest');
-
-// cities
-$app->get('/cities','getAllCities');
-$app->get('/cities/:id/areas','getAllCityAreas');
-
-// location
-$app->get('/location/:id','getLocationDetails');
-$app->get('/category','getCategories');
-$app->get('/search','getSearchResults');
-$app->get('/interest','getInteresredServices');
-
-
-// service provider services
-$app->post('/services', 'insertNewServices');
-$app->post('/service_provider/:id/services', 'insertServices');
-
-$app->post('/mobac/:mobile','insertCallLogs');
-$app->get('/mobac','getCallLogs');
-$app->get('/mobac/recent','getRecentCall');
-$app->put('/mobac/:id','updateCallLogs');
+//$app->get('/service_provider','getServiceProviderByType');
 
 /* Ending Routes */
 
